9b77eb8ffbfeaaa3e594166c74ae32dec0b3331a
fyodorovv <v.fyodorov@list.ru>
2023-09-18 17:56:54 +0300

    hw1

---
+++ hw1/declarative.py
@@ -0,0 +1,6 @@
+def sort_desc(numbers):
+    return sorted(numbers, reverse=True)
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+sorted_numbers = sort_desc(numbers)
+print(sorted_numbers)
---
+++ hw1/imperative.py
@@ -0,0 +1,10 @@
+def bubble_sort_desc(numbers):
+    n = len(numbers)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if numbers[j] < numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort_desc(numbers)
+print(numbers)
