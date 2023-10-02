97d137919cb5f859cbbf88fa0992087ca5b8778c
fyodorovv <v.fyodorov@list.ru>
2023-10-02 20:17:00 +0300

    hw6

---
+++ hw6/binary_search.py
@@ -0,0 +1,21 @@
+# Программа использует императивную, структурную и процедурную парадигмы программирования
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return None
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 7
+result = binary_search(arr, target)
+print(result)
